29610a1e3e2633970823746569f393a07167e5df
Shorty <pppppppman@gmail.com>
2013-05-07 11:49:57 +1000

    First attempt at fixing dates on the Hertz receipt page

---
+++ userscripts/hertz-date-fix/hertz-date-fix.user.js
@@ -0,0 +1,18 @@
+
+// ==UserScript==
+// @name		Shorty Hertz date fixer
+// @namespace	Shorty
+// @description	Fixes the dates on the Hertz receipt request page
+// @include		https://www.hertz.com.au/rentacar/receipts/listReceipts.do
+// ==/UserScript==
+
+var regex = /([0-9]+)\/([0-9]+)\/([0-9]+)/;
+var fdhs = document.getElementsByClassName("formDateHeader");
+for (var i = 0; i < fdhs.length; ++i)
+{
+	if (regex.test(fdhs[i].innerHTML))
+	{
+		var match = regex.exec(fdhs[i].innerHTML);
+		fdhs.innerHTML = match[2] + "/" + match[1] + "/" + match[3];
+	}
+}
